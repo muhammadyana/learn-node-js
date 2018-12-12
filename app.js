e9f4e3fc14305df2340146da7be8365bb07d6fa6
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-12-12 14:53:51 +0700

    :tractor: clean code by passing routes :tada:

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = 3002
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 // app.use((req, res, next) => {
 //   console.log(' In the middleware');
@@ -9,20 +11,8 @@ const port = 3002
 // });
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-  // console.log(' In the another middleware');
-  res.send('<form action="/product" method="POST"> <input type="text" name="title"> <button type="submit" value="Submit"> Submit </button> </form> ');
-});
-
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  // console.log(' In the another middleware');
-  res.send('<h1> Hallo there </h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 
 app.listen(port, () => {
